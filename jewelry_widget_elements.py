e8551befb475b447ec22cbf5084a8f6a0a19f135
viktoryest <67237779+viktoryest@users.noreply.github.com>
2023-05-25 23:41:18 +0400

    Text style edits

--- jewelry_widget_elements.py
+++ jewelry_widget_elements.py
@@ -26,13 +26,13 @@ def create_jewelry_title_1(parent, data, font_20):
     jewelry_title_1 = QtWidgets.QTextEdit(parent=parent)
     jewelry_title_1.setGeometry(QtCore.QRect(583, 229, 1174, 36))
     jewelry_title_1.setStyleSheet("background: transparent; qproperty-textInteractionFlags: NoTextInteraction;")
-    jewelry_title_1.setText(data['title_1'])
-    jewelry_title_1.setAlignment(Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignVCenter)
     jewelry_title_1.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
     jewelry_title_1.setTextColor(QColor(73, 64, 69))
     jewelry_title_1.setFont(font_20)
     jewelry_title_1.setObjectName("jewelry_title_1")
     jewelry_title_1.setReadOnly(True)
+    jewelry_title_1.setText(data['title_1'])
+    jewelry_title_1.setAlignment(Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignVCenter)
     return jewelry_title_1
 
 
@@ -41,13 +41,13 @@ def create_jewelry_main_text_1(parent, data, font_16):
     jewelry_main_text_1.setGeometry(QtCore.QRect(588, 270, 1201, 390))
     jewelry_main_text_1.setStyleSheet("background: transparent; border: 0;"
                                       "qproperty-textInteractionFlags: NoTextInteraction;")
-    jewelry_main_text_1.setText(data['main_text_1'])
-    jewelry_main_text_1.setAlignment(Qt.AlignmentFlag.AlignJustify | Qt.AlignmentFlag.AlignJustify)
-    jewelry_main_text_1.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
     jewelry_main_text_1.setTextColor(QColor(73, 64, 69))
     jewelry_main_text_1.setFont(font_16)
     jewelry_main_text_1.setObjectName("jewelry_main_text_1")
     jewelry_main_text_1.setReadOnly(True)
+    jewelry_main_text_1.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
+    jewelry_main_text_1.setText(data['main_text_1'])
+    jewelry_main_text_1.setAlignment(Qt.AlignmentFlag.AlignJustify | Qt.AlignmentFlag.AlignJustify)
     return jewelry_main_text_1
 
 
@@ -55,13 +55,13 @@ def create_jewelry_title_2(parent, data, font_20):
     jewelry_title_2 = QtWidgets.QTextEdit(parent=parent)
     jewelry_title_2.setGeometry(QtCore.QRect(603, 686, 1174, 36))
     jewelry_title_2.setStyleSheet("background: transparent; border: 0; qproperty-textInteractionFlags: NoTextInteraction;")
-    jewelry_title_2.setText(data['title_2'])
-    jewelry_title_2.setAlignment(Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignVCenter)
     jewelry_title_2.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
     jewelry_title_2.setTextColor(QColor(73, 64, 69))
     jewelry_title_2.setFont(font_20)
     jewelry_title_2.setObjectName("jewelry_title_2")
     jewelry_title_2.setReadOnly(True)
+    jewelry_title_2.setText(data['title_2'])
+    jewelry_title_2.setAlignment(Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignVCenter)
     return jewelry_title_2
 
 
@@ -70,12 +70,12 @@ def create_jewelry_main_text_2(parent, data, font_16):
     jewelry_main_text_2.setGeometry(QtCore.QRect(588, 737, 1201, 220))
     jewelry_main_text_2.setStyleSheet("background: transparent; border: 0; "
                                       "qproperty-textInteractionFlags: NoTextInteraction;")
-    jewelry_main_text_2.setText(data['main_text_2'])
-    jewelry_main_text_2.setAlignment(Qt.AlignmentFlag.AlignJustify)
     jewelry_main_text_2.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
     jewelry_main_text_2.setTextColor(QColor(73, 64, 69))
     jewelry_main_text_2.setFont(font_16)
     jewelry_main_text_2.setObjectName("jewelry_main_text_2")
     jewelry_main_text_2.setReadOnly(True)
+    jewelry_main_text_2.setText(data['main_text_2'])
+    jewelry_main_text_2.setAlignment(Qt.AlignmentFlag.AlignJustify)
     return jewelry_main_text_2
 
